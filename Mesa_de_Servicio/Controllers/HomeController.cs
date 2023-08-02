86d2dfd130fb3ccfba69a2ac862ddad19c743474
Omar031 <52794607+Omar031@users.noreply.github.com>
2023-08-02 18:35:53 -0400

    Update HomeController.cs
    Cambios realizados en el controlador de Home

--- Mesa_de_Servicio/Controllers/HomeController.cs
+++ Mesa_de_Servicio/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Mesa_de_Servicio.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Traducir a prueba3";
+            ViewBag.Message = "Traducir a prueba4";
 
             return View();
         }
